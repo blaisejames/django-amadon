b2dd0521b3a9e4191f4d1afbaae6e4b08268ddbb
Blaise James <blaise@blaisejames.com>
2018-06-16 18:58:31 -0400

    first commit of amadon

---
+++ apps/amadon/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+def index(request):
+    context = {
+        "count": request.session.get('count', 0),
+        "total": request.session.get('total', 0),
+        "grand_total": request.session.get("grand_total", 0),
+        "product_data": [ 
+            { "id": 0, "item": "Dojo Tshirt", "price": 19.99 },
+            { "id": 1, "item": "Dojo Sweater", "price": 29.99 },
+            { "id": 2, "item": "Dojo Cup", "price": 4.95 },
+            { "id": 3, "item": "Algorithm Book", "price": 19.99},
+        ],
+    }
+    request.session['cart'] = context
+    return render(request, 'amadon/index.html', context)
+
+def buy(request):
+    if request.method == "GET":
+        return render(request, "amadon/add.html")
+    elif request.method == "POST":
+        id = int(request.POST['id'])
+        quantity = int(request.POST['quantity'])
+        price = request.session['cart']['product_data'][id]['price']
+        request.session['cart']['count'] = request.session['cart']['count'] + quantity
+        request.session['cart']['total'] = quantity * price
+        print request.session['cart']['total']
+        request.session['cart']['grand_total'] = request.session['cart']['grand_total'] + request.session['cart']['total']
+        return redirect("checkout")
+        
+
+def checkout(request):
+    print request.session['cart']['total']
+    return render(request,'amadon/checkout.html')
+
+def clear(request):
+    for key in request.session.keys():
+        del request.session[key]
+    return redirect('index')
